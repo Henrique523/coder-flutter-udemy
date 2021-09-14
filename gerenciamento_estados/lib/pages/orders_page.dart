9fde1f8c50fac7d8dee43c012ef07efa61d97979
guilherme <guilherme.hlg.eng@gmail.com>
2021-09-14 15:31:36 -0300

    finaliza modulo de requisicoes http

--- gerenciamento_estados/lib/pages/orders_page.dart
+++ gerenciamento_estados/lib/pages/orders_page.dart
@@ -5,22 +5,57 @@ import 'package:gerenciamento_estados/providers/order_list.dart';
 import 'package:provider/provider.dart';
 
 class OrdersPage extends StatelessWidget {
+
+  Future<void> _refreshOrders(BuildContext context) {
+    return Provider.of<OrderList>(
+      context,
+      listen: false,
+    ).loadOrders();
+  }
+
   @override
   Widget build(BuildContext context) {
-    final OrderList orders = Provider.of(context);
-
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Meus Pedidos.'),
+        title: const Text('Meus Pedidos'),
         centerTitle: true,
       ),
       drawer: AppDrawer(),
-      body: ListView.builder(
-        itemCount: orders.itemsCount,
-        itemBuilder: (context, index) {
-          return OrderComponent(order: orders.items[index]);
+      body: FutureBuilder(
+        future: Provider.of<OrderList>(context, listen: false).loadOrders(),
+        builder: (ctx, snapshot) {
+          if(snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else {
+            return RefreshIndicator(
+              onRefresh: () => _refreshOrders(context),
+              child: Consumer<OrderList>(
+                builder: (ctx, orders, child) {
+                  return ListView.builder(
+                    itemCount: orders.itemsCount,
+                    itemBuilder: (context, index) {
+                      return OrderComponent(order: orders.items[index]);
+                    },
+                  );
+                },
+              ),
+            );
+          }
         },
       ),
+      // body: _isLoading
+      //     ? Center(
+      //         child: CircularProgressIndicator(),
+      //       )
+      //     : RefreshIndicator(
+      //       onRefresh: () => _refreshOrders(context),
+      //       child: ListView.builder(
+      //           itemCount: orders.itemsCount,
+      //           itemBuilder: (context, index) {
+      //             return OrderComponent(order: orders.items[index]);
+      //           },
+      //         ),
+      //     ),
     );
   }
 }
--- gerenciamento_estados/lib/providers/order_list.dart
+++ gerenciamento_estados/lib/providers/order_list.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'dart:math';
 
 import 'package:flutter/cupertino.dart';
+import 'package:gerenciamento_estados/models/cart_item.dart';
 import 'package:gerenciamento_estados/models/order.dart';
 import 'package:gerenciamento_estados/providers/cart.dart';
 import 'package:gerenciamento_estados/utils/constants.dart';
@@ -16,6 +17,37 @@ class OrderList extends ChangeNotifier {
 
   int get itemsCount {
     return items.length;
+  }
+
+  Future<void> loadOrders() async {
+    _items.clear();
+
+    final response = await http.get(
+      Uri.parse('${Constants.ORDER_BASE_URL}.json'),
+    );
+
+    if (response.body == 'null') return;
+
+    Map<String, dynamic> data = jsonDecode(response.body);
+    data.forEach((orderId, orderData) {
+      _items.add(
+        Order(
+          id: orderId,
+          total: orderData['total'],
+          products: (orderData['products'] as List<dynamic>).map((item) {
+            return CartItem(
+              id: item['id'],
+              productId: item['productId'],
+              name: item['name'],
+              quantity: item['quantity'],
+              price: item['price'],
+            );
+          }).toList(),
+          date: DateTime.parse(orderData['date']),
+        ),
+      );
+    });
+    notifyListeners();
   }
 
   Future<void> addOrder(Cart cart) async {
