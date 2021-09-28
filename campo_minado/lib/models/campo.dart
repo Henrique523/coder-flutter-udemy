cc49723a1b00ad705a44261d6563d8fdf9271f8f
guilherme <guilherme.hlg.eng@gmail.com>
2021-09-28 17:45:24 -0300

    continua desenvolvimento de campo minado

--- campo_minado/lib/models/campo.dart
+++ campo_minado/lib/models/campo.dart
@@ -1,3 +1,5 @@
+import 'package:campo_minado/models/explosao_exception.dart';
+
 class Campo {
   final int linha;
   final int coluna;
@@ -12,4 +14,86 @@ class Campo {
     required this.linha,
     required this.coluna,
   });
+
+  bool get minado {
+    return _minado;
+  }
+
+  bool get explodido {
+    return _explodido;
+  }
+
+  bool get aberto {
+    return _aberto;
+  }
+
+  bool get marcado {
+    return _marcado;
+  }
+
+  bool get resolvido {
+    bool minadoEMarcado = minado && marcado;
+    bool seguroEAberto = !minado && aberto;
+
+    return minadoEMarcado || seguroEAberto;
+  }
+
+  int get qtdeMinasNaVizinhanca {
+    return vizinhos.where((element) => element.minado).length;
+  }
+
+  bool get vizinhancaSegura {
+    return vizinhos.every((element) => !element.minado);
+  }
+
+  void adicionarVizinho(Campo vizinho) {
+    final deltaLinha = (linha - vizinho.linha).abs();
+    final deltaColuna = (coluna - vizinho.coluna).abs();
+
+    if (deltaLinha == 0 && deltaColuna == 0) {
+      return;
+    }
+
+    if (deltaLinha <= 1 && deltaColuna <= 1) {
+      vizinhos.add(vizinho);
+    }
+  }
+
+  void abrir() {
+    if (aberto) {
+      return;
+    }
+
+    _aberto = true;
+
+    if(minado) {
+      _explodido = true;
+      throw ExplosaoException();
+    }
+
+    if(vizinhancaSegura) {
+      vizinhos.forEach((element) => element.abrir());
+    }
+  }
+
+  void revelarBombas() {
+    if(minado) {
+      _aberto = true;
+    }
+  }
+
+  void minar() {
+    _minado = true;
+  }
+
+  void alternarMarcacao() {
+    _marcado = !_marcado;
+  }
+
+  void reiniciar() {
+    _aberto = false;
+    _marcado = false;
+    _minado = false;
+    _explodido = false;
+  }
 }
